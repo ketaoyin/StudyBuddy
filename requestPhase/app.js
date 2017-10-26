3d74b64d34142ff2c8a010b365f55b48ff372d28
ketaoyin <ketao.yin@gmail.com>
2017-10-26 18:37:13 -0400

    user.js

--- requestPhase/app.js
+++ requestPhase/app.js
@@ -32,7 +32,7 @@ app.use(function(req,res,next){
     next();
 });
 
-app.use('/', requests);
+app.use('/requests', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- requestPhase/routes/users.js
+++ requestPhase/routes/users.js
@@ -1,9 +1,55 @@
 var express = require('express');
 var router = express.Router();
+var hash = require('object-hash');      // Hasher for userID
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* POST create new user profile */
+router.post('/createProfile', function(req, res) {
+    var db = req.db;
+    var user = req.body;
+    console.log('Creating new user profile: ' + JSON.stringify(user));
+
+    var hashUserId = hash(user);
+
+    /* Append userId, rating, numRatings */
+    user.UserID = hashUserId;
+    user.Rating = 0;
+    user.NumRatings = 0;
+
+    db.collection('UserProfiles', function(err, collection) {
+        collection.insert(user, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('Success: ' + JSON.stringify(result[0]));
+                res.send(result[0]);
+            }
+        });
+    });
+});
+
+/* GET user profile information */
+router.get('/userInfo', function(req, res) {
+    var db = req.db;
+    var loginUserName = req.body;
+    // var loginPassword = login.Password;
+
+
+    // collection.findOne({'UserName' : loginUserName, 'Password' : loginPassword}, function(err, result) {
+    db.collection('userprofiles', function(err, collection) {
+        collection.find(function(err, result) {
+            if (err) {
+                res.send({'error':'an error has occured'});
+            } else {
+                console.log('Success');
+                res.send(result);
+            }
+        });
+    });
+});
+    
 module.exports = router;
