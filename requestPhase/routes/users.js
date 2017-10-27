63ffc67d765661de27255e84219bfef23b935899
ketaoyin <ketao.yin@gmail.com>
2017-10-27 15:03:06 -0400

    Semi-functional users.js

--- requestPhase/routes/users.js
+++ requestPhase/routes/users.js
@@ -9,27 +9,41 @@ router.get('/', function(req, res, next) {
 
 /* POST create new user profile */
 router.post('/createProfile', function(req, res) {
+    /*
+        Req params:
+            userID, classID, latitutde, longitude, radius
+    */
     var db = req.db;
     var user = req.body;
+
     console.log('Creating new user profile: ' + JSON.stringify(user));
 
     var hashUserId = hash(user);
+
+    console.log(hashUserId);
 
     /* Append userId, rating, numRatings */
     user.UserID = hashUserId;
     user.Rating = 0;
     user.NumRatings = 0;
 
-    db.collection('UserProfiles', function(err, collection) {
-        collection.insert(user, function(err, result) {
-            if (err) {
-                res.send('An error has occurred');
-            } else {
-                console.log('Success: ' + JSON.stringify(result[0]));
-                res.send(result[0]);
-            }
-        });
-    });
+    var userID = hashUserId;
+    var classID = user.classID;
+
+    console.log("User ID: " + userID);
+    console.log("Class ID: " + classID);
+
+    // var collection = db.get('UserProfiles');
+
+    // collection.insert(user, function(err, result) {
+    //     if (err) {
+    //         res.send('An error has occurred');
+    //     } else {
+    //         console.log('Success: ' + JSON.stringify(result[0]));
+    //         res.send(result[0]);
+    //     }
+    // });
+
 });
 
 /* GET user profile information */
@@ -38,6 +52,7 @@ router.get('/userInfo', function(req, res) {
     var loginUserName = req.body;
     // var loginPassword = login.Password;
 
+    console.out(loginUserName);
 
     // collection.findOne({'UserName' : loginUserName, 'Password' : loginPassword}, function(err, result) {
     db.collection('UserProfiles', function(err, collection) {
