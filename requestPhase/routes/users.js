7759324eeab05234725c6b2b5681c2ec20c25a68
ketaoyin <ketao.yin@gmail.com>
2017-10-27 15:54:22 -0400

    Functional /createProfile

--- requestPhase/routes/users.js
+++ requestPhase/routes/users.js
@@ -11,60 +11,69 @@ router.get('/', function(req, res, next) {
 router.post('/createProfile', function(req, res) {
     /*
         Req params:
-            userID, classID, latitutde, longitude, radius
+            username, password, year, major, name
     */
     var db = req.db;
     var user = req.body;
 
     console.log('Creating new user profile: ' + JSON.stringify(user));
 
+    /* Append userId, rating, numRatings */
+
     var hashUserId = hash(user);
 
-    console.log(hashUserId);
+    var newUser = {
+        "UserID" : hashUserId,
+        "Rating" : "0",
+        "Year" : user.year,
+        "Major" : user.major,
+        "NumRatings" : "0",
+        "Password" : user.password,
+        "UserName" : user.username,
+        "Name" : user.name
+    };
 
-    /* Append userId, rating, numRatings */
-    user.UserID = hashUserId;
-    user.Rating = 0;
-    user.NumRatings = 0;
+    var collection = db.get('UserProfiles');
 
-    var userID = hashUserId;
-    var classID = user.classID;
-
-    console.log("User ID: " + userID);
-    console.log("Class ID: " + classID);
-
-    // var collection = db.get('UserProfiles');
-
-    // collection.insert(user, function(err, result) {
-    //     if (err) {
-    //         res.send('An error has occurred');
-    //     } else {
-    //         console.log('Success: ' + JSON.stringify(result[0]));
-    //         res.send(result[0]);
-    //     }
-    // });
+    collection.insert(newUser, function(err, result) {
+        if (err) {
+            console.log('createProfile: An error has occurred')
+            res.send('An error has occurred');
+        } else {
+            console.log('createProfile: Success');
+            res.send('Success');
+        }
+    });
 
 });
 
 /* GET user profile information */
 router.get('/userInfo', function(req, res) {
     var db = req.db;
-    var loginUserName = req.body;
-    // var loginPassword = login.Password;
+    var loginUserName = req.params.username;
+    var loginPassword = req.params.password;
 
-    console.out(loginUserName);
+    console.log(loginUserName);
+    console.log(loginPassword);
+    
+    var collection = db.get('UserProfiles');
 
-    // collection.findOne({'UserName' : loginUserName, 'Password' : loginPassword}, function(err, result) {
-    db.collection('UserProfiles', function(err, collection) {
-        collection.find(function(err, result) {
-            if (err) {
-                res.send({'error':'an error has occured'});
-            } else {
-                console.log('Success');
-                res.send(result);
-            }
-        });
+    // collection.findOne({'UserName' : loginUserName, 'Password' : loginPassword},{}, function(err, result) {
+    //     if (err) {
+    //         res.json({'error':'an error has occured'});
+    //     } else {
+    //         var userInfo = {
+
+    //         };
+
+    //         res.json(result);
+    //     }
+    // });
+
+    collection.find({},{},function(e,docs){
+        res.json(docs);
     });
+
 });
     
 module.exports = router;
