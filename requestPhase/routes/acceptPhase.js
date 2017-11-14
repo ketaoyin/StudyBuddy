741baaaba177b7e03e292dddc682b7d2bcff3f16
ketaoyin <ketao.yin@gmail.com>
2017-11-14 13:50:41 -0500

    Added global var for server IP

--- requestPhase/routes/acceptPhase.js
+++ requestPhase/routes/acceptPhase.js
@@ -3,6 +3,7 @@ var express = require('express');
 var router = express.Router();
 var ioServer = require('socket.io');
 var ioClient = require('socket.io-client');
+var serverIP = "http://128.61.122.206";
 
 router.get('/', function(req, res, next) {
   res.json('Accept Phase');
@@ -14,66 +15,71 @@ router.get('/invitePairReq', function(req, res, next) {
     var collection = db.get('UserRequests');
     var myID = req.query.myid;
     var userID = req.query.userid;
-    console.log(userID);
-    console.log('my id' + myID);
+    console.log('My ID: ' + myID);
+    console.log('Matchee ID: ' + userID);
 
     //check if user B exists
     collection.find({"UserID": userID}, {$exists: true}, function(err, doc)  {
+	    if(err)
+	    	throw err;
 
-    if(err)
-    	throw err;
+	    console.log('Matchee record found: ' + JSON.stringify(doc));
 
-    if(doc && doc[0].Status == 'Active') {
+	    if(doc && doc[0].Status == 'Active') {
+	    	console.log('Matchee is active')
 
-	//fetch user A info to send to B
-   	collection.aggregate([ {$lookup:
-       {
-         from: 'UserProfiles',
-         localField: 'UserID',
-         foreignField: 'UserID',
-         as: 'info'
-       }
-     },
-     {
-     	$match: {"UserID" : myID}
-     },
-      {
-        $unwind : "$info"
-     }], function(err,result) {
+			//fetch user A info to send to B
+		   	collection.aggregate([ 
+		   		{$lookup:
+			        {
+			        	from: 'UserProfiles',
+			        	localField: 'UserID',
+			        	foreignField: 'UserID',
+			        	as: 'info'
+			        }
+		    	},
+		     	{
+		     	$match: {"UserID" : myID}
+		     	},
+		      	{
+		        $unwind : "$info"
+		     	}], function(err,result) {
 
-     	if(err)
-    	    throw err;
-    	  console.log(result)
-    	  var userInfo = {
-                "Name" : result[0].info.Name,
-                "Rating" : result[0].info.Rating,
-                "Year" : result[0].info.Year,
-                "Major" : result[0].info.Major,
-                "Location" : result[0].loc.coordinates,
-            }
+		     	if(err)
+		    	    throw err;
+		    	
+		    	var userInfo = {
+	                "Name" : result[0].info.Name,
+	                "Rating" : result[0].info.Rating,
+	                "Year" : result[0].info.Year,
+	                "Major" : result[0].info.Major,
+	                "Location" : result[0].loc.coordinates,
+		        }
 
-    //look up which port user B is listening and send myInfo
-    db.get('userIDPort').findOne({"UserID": userID}, function(err, document) {
+		        console.log('Matchee info package: ' + JSON.stringify(userInfo))
 
-    var server = ioServer.listen(document.Port);
+			    //look up which port user B is listening and send myInfo
+			    db.get('userIDPort').findOne({"UserID": userID}, function(err, document) {
 
-	server.on("connection", (socket) => {
-    console.info(`Client connected [id=${socket.id}]`);
+				    var server = ioServer.listen(document.Port);
+					
+					server.on("connection", (socket) => {
+					    console.log(`Client connected [id=${socket.id}]`);
 
-    socket.emit("msg", userInfo);
+					    socket.emit("msg", userInfo);
 
-    socket.on("disconnect", () => {
-        console.info(`Client gone [id=${socket.id}]`);
-	    });
-	});
-	});
-   }); 
+					    socket.on("disconnect", () => {
+					        console.log(`Client gone [id=${socket.id}]`);
+						});
+					});
+				});
+		   }); 
 
-    res.json("Information sent to Client!Please wait for response");
-  }
+	    res.json("Information sent to Client!Please wait for response");
+	  }
 
-   else
-   	res.json("User not active anymore! Please choose another Buddy!");
+	   else
+	   	res.json("User not active anymore! Please choose another Buddy!");
  });
 });
 
@@ -84,17 +90,10 @@ router.get('/receiveMsgFromServer', function(req, res, next) {
    var userID = req.query.userid;
    
    req.db.get('userIDPort').findOne({"UserID": userID}, function(err, document) {
-   
-   console.log("Here!!!!")
 
-   var clientMsg = ioClient.connect("http://128.61.116.87:" + document.Port);
-   
-   console.log("HERE!!!!")
+	   var clientMsg = ioClient.connect(serverIP + ":" + document.Port);
 
-   clientMsg.on("msg", (msg) => res.json(msg));
-
-   console.log("Got it!!!!")
-
+	   clientMsg.on("msg", (msg) => res.json(msg));
    });	
 });
 
@@ -110,6 +109,7 @@ router.get('/respondToPairReq', function(req, res, next) {
 
 	    var server = ioServer.listen(document.Port);
 
+	    console.log("User ID: " + req.query.userid + " Port: " + document.Port)
 		server.on("connection", (socket) => {
 	    console.info(`Client connected [id=${socket.id}]`);
 
@@ -124,11 +124,8 @@ router.get('/respondToPairReq', function(req, res, next) {
 		res.json("Congrats! You've been paired with User A");
 
 		db.get('UserRequests').update({"UserID": req.query.userid}, {$set : {"Status" : "Grouped"}});
-
-		// Group Leader remains active in the Requests Table
-		// db.get('UserRequests').update({"UserID": req.query.myid}, {$set : {"Status" : "Grouped"}});
+		db.get('UserRequests').update({"UserID": req.query.myid}, {$set : {"Status" : "Grouped"}}); 	
 	}
-		
 
 	else {
 		db.get('userIDPort').findOne({"UserID": req.query.userid}, function(err, document) {
